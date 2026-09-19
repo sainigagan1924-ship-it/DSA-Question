7dfe12bac98c8ad00bde1f158324400086560c79
Gagan Saini <saini.gagan.1924@gmail.com>
2026-09-19 12:14:38 +0530

    Add solution for finding disappeared numbers in array
    Implement solution to find all missing numbers in the array.

---
+++ 448. Find All Numbers Disappeared in an Array.java
@@ -0,0 +1,46 @@
+/*
+448. Find All Numbers Disappeared in an Array
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n
+
+*/
+
+// CODE
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            int val = abs(nums[i]);
+            int index = val - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }   
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] > 0) {
+                ans.push_back(i + 1);
+            }
+        }
+        
+        return ans;
+    }
+};
